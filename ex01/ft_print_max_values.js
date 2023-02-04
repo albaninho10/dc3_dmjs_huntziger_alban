ed99e4df7ca51a9a3f42c7f4d64b1c7a591bec38
Roman Juilliot <romanjuilliot@static-css-csd-241165.business.bouyguestelecom.com>
2023-02-04 10:28:58 +0100

    ex01

---
+++ ex01/ft_print_max_values.js
@@ -0,0 +1,15 @@
+let array = [3, 6, 2, 56, 32, 5, 543, 32]
+
+const findLargest = (array) => {
+    var largest = array[0];
+
+    for (var i = 0; i < array.length; i++) {
+        if (largest < array[i] ) {
+            largest = array[i];
+        }
+    }
+
+    return largest
+}
+
+console.log(findLargest(array))
